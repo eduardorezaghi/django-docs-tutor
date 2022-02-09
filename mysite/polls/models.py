f1f907b32417c00652cd255214d031b20fc55796
Eduardo Rezaghi <eduardo.rezaghi@gmail.com>
2022-02-09 13:53:48 -0300

    Add __str__ method and was_published... method

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,4 +1,6 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Question(models.Model):
@@ -6,8 +8,19 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    def was_published_recently(self):
+        """Method returning a True of False assertion depending
+        if the question was published recently"""
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+    def __str__(self) -> str:
+        return str(self.question_text)
+
 class Choice(models.Model):
     """Model representing a choice"""
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self) -> str:
+        return str(self.choice_text)
