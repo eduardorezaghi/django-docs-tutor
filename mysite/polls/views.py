8150178095867bd2e6dc9a7a690f1131ab46de37
Eduardo Rezaghi <eduardo.rezaghi@gmail.com>
2022-02-11 12:02:09 -0300

    Implement Template-View logic with dynamic render
    Using Django render function to dynamically update a view based on a model's mapping content.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+
+from .models import Question
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello world. You're at the polls index.add()")
+    """View-function for index page"""
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    """View-function for details about a question"""
+    return HttpResponse(f"You're lookin at question {question_id}.")
+
+def results(request, question_id):
+    """View-function for results about a question poll"""
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/details.html')
+
+def vote(request, question_id):
+    """View-function for voting in a question"""
+    response = f"You're voting on question {question_id}."
+    return HttpResponse(response)
