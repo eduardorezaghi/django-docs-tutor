ec866a5f08fa9733264e00e6e54737a8ece56c0d
Eduardo Rezaghi <58532229+eduardorezaghi@users.noreply.github.com>
2022-02-14 13:49:33 -0300

    Refactor code with generic based views
    - Updated polls/urls.py with generic views syntax
    - Refactored view code to use generic views, providing a standardized
    way of creating views
    - Updated imports, removing unused ones
    - Also formatted code with black formatter

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,44 +1,56 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
+
 from .models import Question, Choice
 
 # Create your views here.
-def index(request):
-    """View-function for index page"""
-    latest_question_list = Question.objects.order_by('-pub_date')[:]
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    # Overrides the default generic view <app>/<model_name>_list.html
+    # default generic view behaviour by specifying a template name
+    template_name = "polls/index.html"
 
-def detail(request, question_id):
-    """View-function for details about a question"""
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question Does Not Exist")
-    return render(request, 'polls/detail.html', {'question': question})
+    # Overrides the default context template variable <question_list>
+    # with a custom one
+    context_object_name = "latest_question_list"
 
-def results(request, question_id):
-    """View-function for results about a question poll"""
-    # get_list_or_404 is similar, but filter (query) more than
-    # one object from model and users filter() method instead
-    # of get()
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+    def get_queryset(self):
+        """ "Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
+
+
+class DetailView(generic.DetailView):
+    # Default attribute specifying which model
+    # the generic view will act upon
+    model = Question
+
+    template_name = "polls/detail.html"
+
+
+class ResultsView(generic.DetailView):
+    # Default attribute specifying which model
+    # the generic view will act upon
+    model = Question
+
+    # Overrides the default generic view <app>/<model_name>_detail.html
+    # default generic view behaviour by specifying a template name
+    template_name = "polls/results.html"
+
 
 def vote(request, question_id):
     """View-function for voting in a question"""
+    # Query for a question of given question_id in Question model database
     question = get_object_or_404(Question, pk=question_id)
     try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
     except (KeyError, Choice.DoesNotExist):
-        return render(request, 'polls/detail.html', {
-            'question': question,
-            'error_message': "You didn't select a choice."
-        })
+        return render(
+            request,
+            "polls/detail.html",
+            {"question": question, "error_message": "You didn't select a choice."},
+        )
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
